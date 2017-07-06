058ff17ae0c83d8862484deac47a3a3aa656cb30
Robert Yang <yang.robert.w@gmail.com>
2017-07-06 16:19:08 -0400

    added week01B: HelloWorld, SelfPortrait, SimpleTapGameDemo (Diablo5.cs)

---
+++ week01b/Assets/Diablo5.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// we want to talk to a Text UI component
+using UnityEngine.UI; // this line let's us do it
+
+public class Diablo5 : MonoBehaviour {
+
+	public Text myTextDisplay; // remember: don't forget to assign in Inspector!
+
+	int myPoints;
+	bool isCheatModeEnabled;
+	
+	// Update is called once per frame
+	void Update () {
+	//	myTextDisplay.text = Time.time.ToString(); // change Text display to current time
+
+		// tap SPACE a lot to win
+		// every time you tap SPACE, you get 1 point
+		if (Input.GetKeyDown( KeyCode.Space )) { // use GetKeyDown to force user to release the key
+			// all of these lines of code do the same thing
+		//	myPoints = myPoints + 1;
+			myPoints += 1;
+		//	myPoints++; // increments by one
+		}
+
+		// display the current numbers of points
+		myTextDisplay.text = "WELCOME TO DIABLO 5! GET POINTS TO WIN!\ncurrent points: " + myPoints.ToString();
+
+
+		// press [C] on keyboard to activate Cheat Mode
+		if (Input.GetKeyDown( KeyCode.C )) {
+			isCheatModeEnabled = true;
+		}
+
+		// if cheat mode is on, then automatically give me points every frame
+		if (isCheatModeEnabled == true) {
+			myPoints += 5;
+		}
+
+
+	}
+}
---
+++ week01b/Assets/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// START = code runs once, right when the object spawns
+	void Start () {
+		Debug.Log( "Hello World" ); // print "Hello World" to console
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log( "Bonjour Monde" ); // print Bonjour Monde to console
+	}
+}
