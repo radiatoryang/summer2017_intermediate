39714fbc5478c21e271e8e036be79d4cdccf802b
Robert Yang <yang.robert.w@gmail.com>
2017-07-11 20:38:27 -0400

    week02A: vectorMoveDemo, charControlDemo

---
+++ week02a/Assets/scripts/CharControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour {
+
+	CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// gets our input from Unity's virtual joysticks
+		float horizontal = Input.GetAxis( "Horizontal" ); // A/D on keyboard
+		float vertical = Input.GetAxis( "Vertical" ); // W/S on keyboard
+
+		// apply our input to our character controller
+		cc.Move( new Vector3( horizontal, 0f, vertical ) * 0.5f + Physics.gravity * 0.01f );
+	}
+}
---
+++ week02a/Assets/scripts/VectorMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorMove : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		// GetKeyDown and GetKeyUp refer to "event", good for tapping input
+		// GetKey is for holding down
+
+		// MOVEMENT CODE
+		if (Input.GetKey( KeyCode.W )) { // move forward if I press [W]
+			// moved cube while IGNORING rotation
+			// transform.position += new Vector3(0f, 0f, 1f); // "WORLD SPACE"
+
+			// move cube while following rotation
+			transform.Translate( 0f, 0f, 1f ); // "LOCAL SPACE"
+		}
+
+		// TURNING CODE
+		if (Input.GetKey( KeyCode.A )) { // if I hold [A] button...
+			transform.Rotate( 0f, -15f, 0f ); // ... rotate 15 degrees left on Y axis
+		}
+		if (Input.GetKey( KeyCode.D )) { // if I hold [D] button...
+			transform.Rotate( 0f, 15f, 0f ); // ... rotate 15 degrees right on Y axis
+		}
+
+
+	}
+}
