0eec53b43f68f6589e99a51868ba9ed43d89ef44
Robert Yang <yang.robert.w@gmail.com>
2017-07-26 12:41:56 -0400

    week3B: raycasting (roomba drone AI, mouse cursor painting)

---
+++ week03b_raycasting/Assets/scripts/PaintingRaycast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintingRaycast : MonoBehaviour {
+
+	public Transform paintGlob; // the thing I'm painting with
+
+	void Update () {
+		// 1. construct the ray based on mouse cursor position on screen
+		Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+
+		// 2. reserve space in memory to store raycast impact info
+		RaycastHit rayHit = new RaycastHit(); // right now, this is a blank variable
+
+		// optional: visualize the raycast
+		Debug.DrawRay( ray.origin, ray.direction * 100f, Color.yellow );
+
+		// 3. shoot the raycast
+		if (Physics.Raycast( ray, out rayHit, 100f )) {
+			// 4. do stuff with our paintGlob
+			// paintGlob.position = rayHit.point; // RaycastHit.point = world position where the raycast hit a thing
+		
+			// 5. instantiate a new paintGlob clone where the raycast hit
+			Instantiate( paintGlob, rayHit.point, Quaternion.Euler(0f, 0f, 0f) );
+		}
+	}
+}
---
+++ week03b_raycasting/Assets/scripts/RoombaRaycast.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this will be a simple Roomba bot AI steering script to practice raycasting
+public class RoombaRaycast : MonoBehaviour {
+	
+	void Update () {
+		// 1. raycast in front of us...
+
+		// 1a. construct the ray
+		Ray ray = new Ray( transform.position, transform.forward );
+		Debug.DrawRay( ray.origin, ray.direction * 3f, Color.yellow ); // visualize raycast
+
+		// 1b. shoot the raycast for 3 units
+		if (Physics.Raycast( ray, 3f )) {
+			// 2. if the raycast was TRUE, there's a wall in front, so randomly turn left or right
+			float randomNumber = Random.Range( 0f, 100f); // a random number from 0-100
+			if (randomNumber < 50f) { // 50% chance to turn left
+				transform.Rotate( 0f, -90f, 0f );
+			} else { // 50% chance to turn right
+				transform.Rotate( 0f, 90f, 0f );
+			}
+		} else {
+			// 3. else, if the raycast was FALSE, there is nothing in front of us, so move forward
+			transform.Translate( 0f, 0f, 5f * Time.deltaTime ); // Time.deltaTime makes it framerate independent
+		}
+
+	}
+}
